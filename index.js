8133a6e259d50becdae3ab3bf726d0381f9cf20a
Fran Lurbe <francisco.lurbe@gmail.com>
2022-09-30 10:36:33 +0200

    avances

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 document.querySelector('form').addEventListener('submit', event => {
    event.preventDefault();
-   const input = document.querySelector('input');
-   console.log(input.value);
+
+   const input = document.querySelector('#message-input');
+   const encrypted = btoa(input.value);
+
+   document.querySelector('#link-input').value = encrypted;
+   const encryptedInput = document.querySelector('#link-input');
+   console.log(input.value, linkInput.value);
+
+   const decrypted = atob(encryptedInput.value);
+   document.querySelector('#link-decrypted-input').value = decrypted;   
 })
