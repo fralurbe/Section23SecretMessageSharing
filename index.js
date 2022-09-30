8f34224ef03de063554ab66dd003ed3271081f19
Fran Lurbe <francisco.lurbe@gmail.com>
2022-09-30 09:23:49 +0200

    Avances

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-   <meta charset="UTF-8" />
-   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-   <meta name="viewport" content="width=device-width, initial-scale=1.0" />   
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" />
-   <title>Document</title>
-</head>
-<body>
-<script src="index.js"></script>   
-</body>
-</html>
+document.querySelector('form').addEventListener('submit', event => {
+   event.preventDefault();
+   const input = document.querySelector('input');
+   console.log(input.value);
+})
